f304f43f23a875d716aade502713c3904a4e2413
Mpra990 <mpra1210@gmail.com>
2024-09-30 19:16:18 -0300

    first commit

---
+++ j.js
@@ -0,0 +1,8 @@
+header{
+    max-width: 100%;
+    height: 90vh;
+    background: url(images/wind-power-plant-solar-pannels-low-angle.jpg);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    position: relative;
