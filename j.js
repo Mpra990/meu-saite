87a8e1fb280ff21153823323b859bf6eccb4f266
Mpra990 <mpra1210@gmail.com>
2024-09-30 19:41:28 -0300

    Delete j.js

--- j.js
+++
@@ -1,8 +0,0 @@
-header{
-    max-width: 100%;
-    height: 90vh;
-    background: url(images/wind-power-plant-solar-pannels-low-angle.jpg);
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-    position: relative;
